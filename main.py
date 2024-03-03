e68f5776dcc0e6de3a6cf426014e479a75b9ac03
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-03 07:18:39 +0000

    Final Final submission
    I really tried

--- main.py
+++ main.py
@@ -2,21 +2,18 @@ num = input('Enter a number (decimal or integer): ')
 
 
 # type your code here
+
 num1 = num.replace(".", "")
-num2 = num1.lstrip("0").lstrip(" ")
+print(num1)
 
 
 if num1.isdigit() == False or num.count(".") > 1 or num[0] == ".":
   print("Make sure that the number you typed is a valid decimal number")  
-  num = "is invalid,so it" 
-  sf = "an uncalculable number of"
+  num = "ERROR!!!!!!!!!!!"
+  sf = "ERROR!!!!!!!!!!!!!!"
 else:
-  num = num.lstrip("0")
-  if num[0] == ".":
-    num = "0" + num
-    sf = len(num2)
-  else:
-    sf = len(num2)
+  num1 = num1.lstrip("0")
+  sf = len(num1)
  
 # do not change any code beyond this point
 
