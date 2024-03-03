f2c34429d3ede35c39b155454af067f43b3b1cf9
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-03 06:03:58 +0000

    Final version of my code
    .

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 num = input('Enter a number (decimal or integer): ')
 # type your code here
+num1 = num.replace(".", "")
+num2 = num1.lstrip("0").lstrip(" ")
 
-
-
+if num1.isdigit() == False or num.count(".") > 1:
+ print("Make sure that the number you typed is a valid decimal number")  
+ num = "is invalid,so it" 
+ sf = "an uncalculable number of"
+else:
+ num = num.lstrip("0")
+ sf = len(num2)
 # do not change any code beyond this point
+#I really need the spacing :(
 print('The number', num, 'has', sf, 'significant figures.')
