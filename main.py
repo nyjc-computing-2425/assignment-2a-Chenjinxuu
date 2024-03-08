a3fdc6c7039314f211a5ceb3037a3557a1e69aa7
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-08 05:33:41 +0000

    Updated Final Final submission
    w

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ num = input('Enter a number (decimal or integer): ')
 
 
 # type your code here
-
-num1 = num.replace(".", "")
-print(num1)
-
-
-if num1.isdigit() == False or num.count(".") > 1 or num[0] == ".":
-  print("Make sure that the number you typed is a valid decimal number")  
-  num = "ERROR!!!!!!!!!!!"
-  sf = "ERROR!!!!!!!!!!!!!!"
-else:
-  num1 = num1.lstrip("0")
-  sf = len(num1)
+num = num.strip(" ")
+num1 = num.replace(".", "") 
+num1 = num1.lstrip("0")  
+sf = len(num1)
  
 # do not change any code beyond this point
 
